bd06ccb70cb265b762bee744534c004b3ebcc2b9
ARCHITTOMAR15 <architchoudhary009@gmail.com>
2026-09-12 11:20:59 +0530

    finalize Streamlit Cloud deployment for AI Meeting Notes Analyzer

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -4,6 +4,8 @@ import sys
 
 from langchain_community.llms import LlamaCpp
 
+from functools import lru_cache
+from huggingface_hub import hf_hub_download
 from src.config.config import load_config
 from src.utils.exception import ProjectException
 from src.utils.logger import get_logger
@@ -13,17 +15,24 @@ logger = get_logger(__name__)
 
 class MeetingLLM:
     @classmethod
+    @lru_cache(maxsize=1)
     def load_model(cls) -> LlamaCpp:
 
         try:
             config = load_config()
             llm_config = config["llm"]
-            model_path = llm_config["model_path"]
+            #model_path = llm_config["model_path"]
 
-            if not os.path.exists(model_path):
-                raise FileNotFoundError(
-                    f"GGUF model not found at: {model_path}"
-                )
+            #if not os.path.exists(model_path):
+                #raise FileNotFoundError(
+                    #f"GGUF model not found at: {model_path}"
+                #)
+            model_path = hf_hub_download(
+                  repo_id="Qwen/Qwen2.5-3B-Instruct-GGUF",
+                  filename="qwen2.5-3b-instruct-q4_k_m.gguf",
+                  cache_dir=os.getenv("HF_HOME", "/tmp/huggingface"),)
+
+
 
             llm = LlamaCpp(
                 model_path=model_path,
