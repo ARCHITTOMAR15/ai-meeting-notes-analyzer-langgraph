51543b8c10eb9a3d0308f1880980be2cc98521c3
ARCHITTOMAR15 <architchoudhary009@gmail.com>
2026-09-14 06:26:27 +0530

    Fix streamlit installation

--- src/preprocessing/cleaner.py
+++ src/preprocessing/cleaner.py
@@ -1,29 +1,34 @@
 
-
 import re
 import sys
-from llama_index.core.schema import Document
+
 from src.utils.exception import ProjectException
 from src.utils.logger import get_logger
-logger=get_logger(__name__)
+
+logger = get_logger(__name__)
 
 
 class TranscriptCleaner:
+
     @staticmethod
-    def clean(document:Document)->Document:
-
+    def clean(document):
         try:
-            text=document.text.strip()
+            # Lazy import (important for Streamlit Cloud)
+            from llama_index.core.schema import Document
 
-            text=re.sub(r"\n{3,}", "\n\n", text)
+            text = document.text.strip()
 
-            cleaned_document=Document(text=text,metadata=document.metadata.copy())
+            text = re.sub(r"\n{3,}", "\n\n", text)
+            text = re.sub(r"[ \t]+", " ", text)
 
-            logger.info("Transcript Normalized Sucessfully")
+            cleaned_document = Document(
+                text=text,
+                metadata=document.metadata.copy() if document.metadata else {},
+            )
 
+            logger.info("Transcript cleaned successfully.")
             return cleaned_document
 
         except Exception as e:
             logger.error(str(e))
-            raise ProjectException(str(e), sys)
-
+            raise ProjectException(e, sys)
